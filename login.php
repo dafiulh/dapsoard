2d073f76b9fb6108dab19c93c99ce79340eb193e
Dafiul Haq <dafiulhaq97@gmail.com>
2020-07-01 10:18:47 +0700

    password disimpan di file "password"

--- login.php
+++ login.php
@@ -1,9 +1,14 @@
 <?php
+$password_file = "./password";
+$file = fopen($password_file, "r");
+$password = fread($file,filesize($password_file));
+fclose($file);
+
 if (isset($_COOKIE["login"])) {
     header("Location: index.php");
 }
 if (isset($_POST["submit"])) {
-    if($_POST["password"] == "iluzyimbf95"){
+    if($_POST["password"] == $password){
         setcookie("login",md5($_POST["password"]));
         header("Location: index.php");
     }
@@ -33,9 +38,15 @@ $name="Login";
                    <p class="text-muted">Enter admin passsword</p>
                 </div>
                 <form method="post">
-                    <div class="form-group"><input type="password" name="password" class="form-control rounded border" placeholder="Password" value="<?= $password ?>"></div>
-                    <?php if(isset($_POST["submit"]) && $_POST["password"] != "iluzyimbf95"): ?><p class="text-danger">Wrong password</p><?php endif; ?>
-                    <div class="form-group mt-4"><input type="submit" name="submit" value="Submit" class="btn btn-dark rounded"></div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control rounded border" placeholder="Password">
+                    </div>
+                    <?php if(isset($_POST["submit"]) && $_POST["password"] != $password): ?>
+                    <p class="text-danger">Wrong password</p>
+                    <?php endif; ?>
+                    <div class="form-group mt-4">
+                        <input type="submit" name="submit" value="Submit" class="btn btn-dark rounded">
+                    </div>
                 </form>
             </div>
         </div>
