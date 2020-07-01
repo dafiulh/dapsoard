82e9d17fab230ee312d91783c772958b8ed03364
Dafiul Haq <dafiulhaq97@gmail.com>
2020-07-01 09:48:38 +0700

    ganti menu item jadi lebih umum

--- parts/sidebar.php
+++ parts/sidebar.php
@@ -12,21 +12,22 @@
                 <li class="nav-label">Skills</li>
                 <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-language"></i><span class="hide-menu">English</span></a>
                     <ul aria-expanded="false" class="collapse">
-                        <li><a href="vocab.php">Kosa Kata</a></li>
-                        <li><a href="#">Latihan</a></li>
+                        <li><a href="vocab.php">Vocabularies</a></li>
+                        <li><a href="#">Exercise</a></li>
                     </ul>
                 </li>
                 <li class="nav-label">Personal</li>
-                <li> <a class="has-arrow  " href="javascript:void(0)" aria-expanded="false"><i class="fa fa-sticky-note-o"></i><span class="hide-menu">Catatan</span></a>
+                <li> <a class="has-arrow  " href="javascript:void(0)" aria-expanded="false"><i class="fa fa-sticky-note-o"></i><span class="hide-menu">Notes</span></a>
                     <ul aria-expanded="false" class="collapse">
-                        <li><a href="#">Masdap Fitur</a></li>
-                        <li><a href="#">Web Server Gratis</a></li>
+                        <li><a href="#">Note 1</a></li>
+                        <li><a href="#">Note 2</a></li>
                     </ul>
                 </li>
                 <li> <a class="has-arrow" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-hdd-o"></i><span class="hide-menu">Backup</span></a>
                     <ul aria-expanded="false" class="collapse">
-                        <li><a href="#">Flot</a></li>
-                        <li><a href="#">Morris</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">Pictures</a></li>
+                        <li><a href="#">Files</a></li>
                     </ul>
                 </li>
             </ul>
