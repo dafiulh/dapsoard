33641d22687ad65c42d6779729c85860325755a0
Dafiul Haq <dafiulhaq97@gmail.com>
2020-07-01 12:27:24 +0700

    pindah css & js ke folder assets

--- parts/head.php
+++ parts/head.php
@@ -8,5 +8,5 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
-    <link href="./main.css" rel="stylesheet">
+    <link href="./assets/template.css" rel="stylesheet">
 </head>
